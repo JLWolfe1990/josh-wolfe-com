96d0f4695964c938e806f3e1840854cde04044e5
Joshua Wolfe <jlwolfe1990@gmail.com>
2023-12-15 08:13:34 -0600

    Add mobile navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,63 @@
 import { NavItem } from "../NavItem/NavItem";
+import barsIcon from "../../assets/icons/bars-solid.svg";
+import xmarkIcon from "../../assets/icons/xmark-solid.svg";
+import {useState} from "react";
 
 interface NavProps {
     page: 'Home' | 'Skills' | 'Experience' | 'FAQ' | 'Articles';
 }
 export function Nav({page}: NavProps) {
+    // use state to track whether the nav is open or closed
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <nav className={`flex-none text-2xl`}>
-            <div className={`flex flex-row`}>
+        <nav className={`flex-none text-sm lg:text-2xl min-h-[88px]`}>
+            <div className={`${isOpen ? 'sm:visible' : 'hidden'} lg:hidden absolute top-0 right-0 w-full p-4 z-10`}>
+                <div
+                    className={`flex flex-col items-center justify-center opacity-100 text-black w-full p-4 border-4 border-pinkish rounded-lg bg-whiteish`}>
+                    <div className={'flex flex-row w-full p-0 justify-end'}>
+                        <div
+                          className={`flex h-14 w-14 justify-end items-start`}
+                          onClick={() => {
+                              console.log('clicked');
+                              setIsOpen(!isOpen)}}
+                        >
+                            <img
+                              src={xmarkIcon}
+                              alt={'navigation close icon'} />
+                        </div>
+                    </div>
+                    <ul className={`flex flex-col m-0 w-full list-none`}>
+                        <li>
+                            <NavItem active={page === 'Home'} label={'Home'} to={`/`} />
+                        </li>
+                        <li>
+                            <NavItem active={page === 'Skills'} label={`Skills`} to={`/skills`} />
+                        </li>
+                        <li>
+                            <NavItem active={page === 'Experience'} label={`Experience`} to={`/experience`} />
+                        </li>
+                        <li>
+                            <NavItem active={page === 'FAQ'} label={`FAQs`} to={`/faq`} />
+                        </li>
+                        <li>
+                            <NavItem label={`Articles`} to={`https://josh-wolfe.medium.com/`} external={true} />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className={`lg:hidden flex flex-row items-center justify-end space-x-6 h-full opacity-90 text-black w-full p-4`}>
+                <div
+                  className={`${isOpen ? 'hidden' : 'visible'} flex h-14 w-14 border-4 border-blueish rounded-full justify-center items-center bg-whiteish`}
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                    <img
+                      src={barsIcon}
+                      alt={'navigation icon'} />
+                </div>
+            </div>
+
+            <div className={`hidden lg:flex flex flex-row`}>
                 <div className={`basis-1/6`}></div>
                 <ul className={`flex flex-row basis-4/6`}>
                     <li className={`grow`}>
--- src/components/NavItem/NavItem.tsx
+++ src/components/NavItem/NavItem.tsx
@@ -14,16 +14,16 @@ export function NavItem({ label, to, active, external }: NavItemProps) {
     const inactiveBgStyles = `bg-whiteish hover:bg-blackish`;
     const bgStyles = active ? activeBgStyles : inactiveBgStyles;
     const activeBorderStyles = `border-blackish hover:border-whiteish border-x-4 lg:border-x-8 border-b-4 lg:border-b-8`;
-    const inactiveBorderStyles = `border-blueish hover:border-blackish border-x-4 lg:border-x-8 border-b-4 lg:border-b-8`;
+    const inactiveBorderStyles = `border-blueish hover:border-blackish border-4 lg:border-t-0 lg:border-x-8 lg:border-b-8`;
     const borderStyles = active ? activeBorderStyles : inactiveBorderStyles;
-    const roundingStyles = `rounded-b-3xl`;
-    const spacingStyles = `px-2 lg:px-4 pb-1 lg:pb-2 pt-0 lg:pt-0`;
+    const roundingStyles = `rounded-lg lg:rounded-none lg:rounded-b-3xl`;
+    const spacingStyles = `p-2 lg:px-4 lg:pb-2 lg:pt-0`;
     const transitionStyles = `transition-colors duration-300 ease-in-out`;
     const transparencyStyles = `opacity-75 hover:opacity-100`;
 
     return (
-        <div className={`flex flex-row justify-center items-center h-full pb-2 lg:pb-6`}>
-            <div className={`${fontStyles} ${bgStyles} ${borderStyles} ${roundingStyles} ${spacingStyles} ${transitionStyles} ${transparencyStyles}`}>
+        <div className={`flex flex-row justify-center items-center text-center h-full pb-4 lg:pb-6`}>
+            <div className={`${fontStyles} ${bgStyles} ${borderStyles} ${roundingStyles} ${spacingStyles} ${transitionStyles} ${transparencyStyles} w-full lg:w-fit`}>
                 { external ? (<Link rel="noopener noreferrer" target="_blank" to={to}>{label}</Link>) : (<Link to={to}>{label}</Link>) }
             </div>
         </div>
